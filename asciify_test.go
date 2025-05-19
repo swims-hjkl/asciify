0fca2eb7649bdc120d1d314d83861851bae1f4c7
swims_hjkl <sumedhrao7@gmail.com>
2025-05-19 19:04:21 +0530

    feat(test): added test case for ascii output

--- asciify_test.go
+++ asciify_test.go
@@ -1,9 +1,27 @@
 package asciify
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestImageToASCII(t *testing.T) {
-	ConvertImageToAscii("testdata/arcreactor.png", 150)
+	basePath := "testdata"
+	testImagePath := filepath.Join(basePath, "portrait_table.jpg")
+	expectedOutputFilePath := filepath.Join(basePath, "expected_output.txt")
+
+	expectedString, err := os.ReadFile(expectedOutputFilePath)
+	if err != nil {
+		t.Fatalf("failed to read expected output file: %v", err)
+	}
+
+	outputString, err := ConvertImageToAscii(testImagePath, 150)
+	if err != nil {
+		t.Fatalf("failed to convert image to ascii: %v", err)
+	}
+
+	if !(outputString == string(expectedString)) {
+		t.Fatal("The expected string does not match the actual string")
+	}
 }
